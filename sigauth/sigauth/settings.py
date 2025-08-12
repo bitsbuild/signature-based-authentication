dcfb3415df7f74c5a6040854741db557759bb803
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-08-13 01:38:09 +0530

    Project Restructuring

--- sigauth/sigauth/settings.py
+++ sigauth/sigauth/settings.py
@@ -14,7 +14,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'user'
+    'api'
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- sigauth/user/urls.py
+++
@@ -1,7 +0,0 @@
-from user.views import create,login,delete
-from django.urls import path
-urlpatterns = [
-    path('create/',create,name='create'),
-    path('login/',login,name='login'),
-    path('delete/',delete,name='delete')
-]
