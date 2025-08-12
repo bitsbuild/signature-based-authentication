2840b7ac18367144d2c1fab3e2dbdbc2969727ae
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-08-12 19:49:05 +0530

    URLs File Written For User

--- sigauth/user/urls.py
+++ sigauth/user/urls.py
@@ -1,3 +1,7 @@
+from user.views import create,login,delete
+from django.urls import path
 urlpatterns = [
-    
+    path('create/',create,name='create'),
+    path('login/',login,name='login'),
+    path('delete/',delete,name='delete')
 ]
