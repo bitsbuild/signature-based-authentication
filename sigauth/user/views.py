9d6d636fb1dbd9d624aa482cfae1fa50a38699cc
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-08-12 19:41:47 +0530

    Basic View Structures Defined

--- sigauth/user/views.py
+++ sigauth/user/views.py
@@ -1,1 +1,10 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+@api_view['POST']
+def create(request):
+    pass
+@api_view['POST']
+def login(request):
+    pass
+@api_view['POST']
+def delete(request):
+    pass
