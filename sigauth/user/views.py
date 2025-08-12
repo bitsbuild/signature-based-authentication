57ad39b520bf7b804358a0d38b9e7801b97d90b3
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-08-13 00:09:09 +0530

    Views Cleared

--- sigauth/user/views.py
+++ sigauth/user/views.py
@@ -4,24 +4,7 @@ from user.serializer import UserSerializer
 from rest_framework.status import HTTP_201_CREATED,HTTP_400_BAD_REQUEST
 @api_view(['POST'])
 def create(request):
-    # Next To Do: Stored Hashed Password And Store Image To Online Storage And Store Link In Instance Here
-    try:
-        data = request.data
-        serializer = UserSerializer(data=data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(
-            {
-                "Status":"Account Created Successfully",
-            },status=HTTP_201_CREATED
-        )
-    except Exception as e:
-        return Response(
-            {
-                "Status":"Error",
-                "Error":str(e)
-            },status=HTTP_400_BAD_REQUEST
-        )
+    pass
 @api_view(['POST'])
 def login(request):
     pass
