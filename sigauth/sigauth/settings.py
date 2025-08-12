273699ad6b71ddffebdeff7300de07083312813d
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-08-13 01:38:35 +0530

    Project Cleanup

--- sigauth/sigauth/settings.py
+++ sigauth/sigauth/settings.py
@@ -14,7 +14,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'api'
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
