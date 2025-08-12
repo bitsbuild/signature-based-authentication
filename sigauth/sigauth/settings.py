38eb3d57b93a6cefd7b4ef25635532a53467e5f8
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-08-13 01:40:37 +0530

    API Django App Added

--- sigauth/sigauth/settings.py
+++ sigauth/sigauth/settings.py
@@ -14,6 +14,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'api'
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
