26595c8501f8f0095ec70b66be8cc34e038a9107
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-08-12 19:30:33 +0530

    User Model Created And Migrations Applied

--- sigauth/user/models.py
+++ sigauth/user/models.py
@@ -1,1 +1,13 @@
-from django.db import models
+from django.db.models import Model,UUIDField,CharField,EmailField,URLField,DateTimeField
+from uuid import uuid4
+class User(Model):
+    id = UUIDField(default=uuid4,editable=False,blank=False,null=False,primary_key=True)
+    name = CharField(max_length=150,blank=False)
+    username = CharField(max_length=25,unique=True,blank=False)
+    password = CharField(max_length=32,blank=False)
+    email = EmailField(unique=True,blank=False)
+    signature = URLField(unique=True,editable=False,blank=False,null=False)
+    created = DateTimeField(auto_now_add=True,editable=False,blank=False,null=False)
+    updated = DateTimeField(auto_now=True,editable=False,blank=False,null=False)
+    def __str__(self):
+        return self.name
