c9559a402283700e84fb301be0055f535aadc46b
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-08-13 09:53:53 +0530

    Pagination Not Required As Again No Get Requests As No Storing

--- sigauth/sigauth/settings.py
+++ sigauth/sigauth/settings.py
@@ -69,6 +69,7 @@ USE_I18N = True
 USE_TZ = True
 STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# ----- CONFIGURATION STARTS HERE -----
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES':[
         'rest_framework.authentication.TokenAuthentication'
